6ee429475e77906b83e5b1bd74b36e4b74d436dd
Rohan Hansda <123rhansda123@gmail.com>
2021-04-08 00:00:08 +0530

    series_1

---
+++ C programming/series_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i, n;
+    float s = 0.00, d = 2.00, m = 1.00;
+    printf("Enter the nth number: ");
+    scanf("%d", &n);
+    for (i = 0; i <= n; i++)
+        ;
+    {
+        s = s + m / d;
+        d = d + 2.00;
+    }
+    printf("Sum of the series is %f", s);
+    return 0;
+}
