7c70b4203796560c62b6622634e2c2a88b6f1c60
Rohan Hansda <123rhansda123@gmail.com>
2021-04-10 15:42:26 +0530

    series-3

---
+++ C programming/series-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,d=0;
+    printf("Enter the nth number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        d=i*i-1;
+        printf("%d ",d);
+    }
+    return 0;
+}
