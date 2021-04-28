0ef5946db23568292a604ffd90cc6f6573a0213e
Rohan Hansda <123rhansda123@gmail.com>
2021-04-28 19:15:19 +0530

    prime-number-between -two-ranges

---
+++ C programming/prime-number-between-two-range.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, i, p, x;
+	printf("Enter two renges: ");
+	scanf("%d", &a, &b);
+	for (i = a; i <= b; i++)
+		;
+	{
+		x = 0;
+	}
+	for (p = 2; p < i; p++)
+	{
+		if (i % p == 0)
+		{
+			x = 1;
+		}
+	}
+	if (x == 0)
+	{
+		printf("%d is Prime Number", i);
+	}
+	return 0;
+}
