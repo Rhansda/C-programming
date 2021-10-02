f470d7c5488ddb38a5d7afdb06ce51d370bff15d
Rohan Hansda <123rhansda123@gmail.com>
2021-10-02 10:54:48 +0530

    factorial.java

---
+++ VS Code_JAVA/fact.java
@@ -0,0 +1,11 @@
+public class fact {
+    public static void main(String[] args) {
+        int n = 12;
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+
+        }
+        System.out.println("factorial of " + " " + n + " is " + fact);
+    }
+}
