2581295d0e6d646a114f48cb65b5345b9b8c27b6
Rohan Hansda <123rhansda123@gmail.com>
2021-11-10 09:17:27 +0530

    prime PrimeNumbersList

---
+++ VS Code_JAVA/tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+class demo1 {
+    public static void main(String)
+    {
+        int a=6;
+        int b=a++;
+        int c=++b;
+        int d=-1;
+        System.out.println("a b c d"+a+b+c+d);
+    }
+}
